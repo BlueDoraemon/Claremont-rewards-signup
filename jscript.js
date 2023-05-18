571c5b9f76505e6674fa5029050baba5c7816d6e
BlueDoraemon <3803480+BlueDoraemon@users.noreply.github.com>
2023-05-19 01:06:22 +1000

    Add password match needs more work

--- jscript.js
+++ jscript.js
@@ -1,37 +1,39 @@
 
 //check for password match
 function confirmPassword(){
-    let a = document.querySelector('#password');
-    
-    let b = document.querySelector('#confirm-password')
-    if (a != ""){
-        return (a === b);
-    }
-    else return false;
+const a = document.querySelector('#password');
+const b = document.querySelector('#confirmpassword')
+    if (a.value.trim() === ""){
+        return false;}
+       else return (a.value === b.value);
 }
 
+function passwordMatch(){
+
+const spanA = document.querySelector(`.spanpassword`);
+const spanB = document.querySelector(`.spanconfirmpassword`);
+const a = document.querySelector('#password');
+const b = document.querySelector('#confirmpassword')
+
+if (confirmPassword()){
+    a.classList.remove('invalid');
+    a.classList.add('valid');
+    b.classList.remove('invalid');
+    b.classList.add('valid');
+}
+else {
+    a.classList.add('invalid');
+    a.classList.remove('valid'); 
+    b.classList.add('invalid');
+    b.classList.remove('valid');    
+      
+    spanA.textContent ="Passwords don't match Try again";
+    spanB.textContent ="Passwords don't match Try again";
+}   
+}
 //on submit check inputs are valid and not empty
 
 //prevent default tooltip
-
-var form = document.getElementById('forms'); 
-
-form.addEventListener('submit', function(event) {
-  var inputs = form.querySelectorAll('input');
-
-  for (var i = 0; i < inputs.length; i++) {
-    var input = inputs[i];
-
-    if (input.checkValidity() && input.value.trim() !== '') {
-      // Input is valid and not empty
-      input.classList.remove('invalid');
-    } else {
-      // Input is invalid or empty
-      input.classList.add('invalid');
-      event.preventDefault(); // Prevent form submission
-    }
-  }
-});
 
 var inputs = document.querySelectorAll('input');
 
@@ -49,10 +51,16 @@ inputs.forEach(function(input) {
   input.addEventListener('change', ()=> {
     const spans = document.querySelector(`.span${input.id}`);
     if (input.checkValidity()) {
-        input.classList.add('valid');
+        //password check
+        if (input.id === 'confirmpassword'){
+            passwordMatch();
+        }
+
+        else { input.classList.add('valid');
         input.classList.remove('invalid');     
         //change text for spans
-        spans.textContent ="";
+        spans.textContent ="";}
+
     }
     else {
     input.classList.add('invalid');
@@ -68,5 +76,4 @@ inputs.forEach(function(input) {
     };
     };
   });
-
 });
